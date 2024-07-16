6d4c159d6bba183055a96b2be9828a7e13935c6e
David Asulin <dudyas6@gmail.com>
2024-07-16 20:43:46 +0300

    שדג

--- src/components/CreateWallet/CreateWallet.js
+++ src/components/CreateWallet/CreateWallet.js
@@ -13,22 +13,22 @@ const CreateWallet = ({ setSelectedComponent }) => {
     <>
       <div className="flex flex-col items-center justify-center gap-10">
         <h1 className="text-center font-bold">Create a Wallet</h1>
-        <form class="space-y-4 font-[sans-serif] max-w-md mx-auto">
+        <form className="space-y-4 font-[sans-serif] max-w-md mx-auto">
           <input
             type="username"
             placeholder="Enter Username"
-            class="px-4 py-3 bg-gray-100 w-full text-sm outline-none border-b-2 border-blue-500 rounded"
+            className="px-4 py-3 bg-gray-100 w-full text-sm outline-none border-b-2 border-blue-500 rounded"
           />
 
           <input
             type="password"
             placeholder="Enter Password"
-            class="px-4 py-3 bg-gray-100 w-full text-sm outline-none border-b-2 border-transparent focus:border-blue-500 rounded"
+            className="px-4 py-3 bg-gray-100 w-full text-sm outline-none border-b-2 border-transparent focus:border-blue-500 rounded"
           />
 
           <button
             type="button"
-            class="!mt-8 w-full px-4 py-2.5 mx-auto block text-sm bg-blue-500 text-white rounded hover:bg-blue-600"
+            className="!mt-8 w-full px-4 py-2.5 mx-auto block text-sm bg-blue-500 text-white rounded hover:bg-blue-600"
           >
             Submit
           </button>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,33 +1,33 @@
 import React from "react";
 
-const Menu = ({setSelectedComponent}) => {
+const Menu = ({ setSelectedComponent }) => {
   return (
     <div className="flex flex-col items-center justify-between  w-full h-full">
       <h1>Select Action</h1>
       <div className="flex flex-col">
         <button
           type="button"
-          class="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
+          className="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
+        >
+          Open Wallet
+        </button>
+        <button
+          type="button"
+          className="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
           onClick={() => setSelectedComponent("CreateWallet")}
         >
           Create Wallet
         </button>
         <button
           type="button"
-          class="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
-        >
-          Open Wallet
-        </button>
-        <button
-          type="button"
-          class="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
+          className="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
         >
           Restore Wallet
         </button>
         {/* <button>Check Balance</button> */}
         <button
           type="button"
-          class="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
+          className="px-5 py-2.5 rounded-lg text-sm tracking-wider font-medium border border-current outline-none bg-blue-700 hover:bg-transparent text-white hover:text-blue-700 transition-all duration-300"
         >
           Send Signed Transaction
         </button>
